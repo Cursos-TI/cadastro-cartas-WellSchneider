b5169a2e7297ada645425674d3d7baa9db6f8d6f
WellSchneider <102704878+WellSchneider@users.noreply.github.com>
2026-04-10 02:21:49 +0000

    Inicializei todas as variaveis em 0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,13 @@ int main() {
     // =========================
 
     // Aqui declaramos as variáveis que irão armazenar os dados da primeira carta
-    char estado1;
-    char codigo1[4];
-    char cidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
+    char estado1 = 0;
+    char codigo1[4] = {0};
+    char cidade1[50] = {0};
+    int populacao1 = 0;
+    float area1 = 0;
+    float pib1 = 0;
+    int pontosTuristicos1 = 0;
 
     // Mensagem inicial para orientar o usuário
     printf("=== Cadastro da Carta 1 ===\n");
@@ -53,13 +53,13 @@ int main() {
     // =========================
 
     // Agora repetimos o processo para a segunda carta
-    char estado2;
-    char codigo2[4];
-    char cidade2[50];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontosTuristicos2;
+    char estado2 = 0;
+    char codigo2[4] = {0};
+    char cidade2[50] = {0};
+    int populacao2 = 0;
+    float area2 = 0;
+    float pib2 = 0;
+    int pontosTuristicos2 = 0;
 
     printf("\n=== Cadastro da Carta 2 ===\n");
 
